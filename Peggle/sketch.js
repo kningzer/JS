44b7490b7dd3f381b00839d33247ee67cb7a7d5e
Daniel Blom <daniel.n.jonsson@outlook.com>
2022-03-29 19:58:29 +0200

    improvments

--- Peggle/sketch.js
+++ Peggle/sketch.js
@@ -1,20 +1,33 @@
 
-console.log('hello world');
+console.log('hello Peggle');
+
+// Basvärden för plattan
+// Plattorna har 29*29 piggar
+var pxBredd = 21;
+var stdPegs = 29
+
+let pegsw =stdPegs;
+let pegsh =stdPegs;
+let bas = stdPegs * pxBredd;
+
 var image = new Image();
 //image.src = ;
-let pegs =58;
 
 var btn2 = document.getElementById('btn2');                     // Open file button
 var btnpeg = document.getElementById('btnpeg');                 // Open file button
 var btnzoompos = document.getElementById('btnzoompos');         // Open file button
 var btnzoomneg = document.getElementById('btnzoomneg');         // Open file button
+var btn1platta = document.getElementById('btn1platta');         // button
+var btn2platta = document.getElementById('btn2platta');         // button
+var btn3platta = document.getElementById('btn3platta');         // button
+var btn4platta = document.getElementById('btn4platta');         // button
 var fIn = document.getElementById('file-input');                // Hidden file input field
 var pixels = null;                                              // Pixel object for manipulating the picture
 var canvas = document.getElementById('canvas1');                // The canvas we are working with
 var isDragging=false;                                           // Dragging, yes or no
 var ctx=null;                                                   // Context holder
-canvas.width = 609*2;
-canvas.height = 609;
+canvas.width = bas;
+canvas.height = bas;
 var offsetX=canvas.offsetLeft;                                  // Offset for dragging
 var offsetY=canvas.offsetTop;                                   // offset for dragging
 
@@ -29,8 +42,41 @@ var moveYAmount = 0;
 var mstartX = 0;
 var mstartY = 0;
 
+//------------------------
 
-let scale = 1;
+btn1platta.addEventListener("click", function() {         // Denna triggar file browser vid klick på knapp
+  console.log("platta 1 event"); 
+  canvas.width = bas;
+  canvas.height = bas;
+  pegsw = 29;
+  pegsw = 29;
+});
+
+btn2platta.addEventListener("click", function() {         // Denna triggar file browser vid klick på knapp
+  console.log("platta 2 event"); 
+  canvas.width = bas * 2;
+  canvas.height = bas;
+  pegsw = stdPegs *2;
+  pegsh = stdPegs;
+});
+
+btn3platta.addEventListener("click", function() {         // Denna triggar file browser vid klick på knapp
+  console.log("platta 3 event"); 
+  canvas.width = bas;
+  canvas.height = bas * 2;
+  pegsw = stdPegs;
+  pegsh = stdPegs * 2;
+});
+
+btn4platta.addEventListener("click", function() {         // Denna triggar file browser vid klick på knapp
+  console.log("platta 3 event"); 
+  canvas.width = bas * 2;
+  canvas.height = bas * 2;
+  pegsw = stdPegs * 2;
+  pegsh = stdPegs * 2;
+});
+
+//------------------------
 
 btnpeg.addEventListener("click", function() {         // Denna triggar file browser vid klick på knapp
   console.log("Btnpeg event"); 
@@ -44,14 +90,14 @@ btn2.addEventListener("click", function() {         // Denna triggar file browse
 
 btnzoompos.addEventListener("click", function() {         // Denna triggar file browser vid klick på knapp
   console.log("Klick zoom pos");
-  currScale = 2;
+  currScale = currScale + 1;
   ScaleAndRedraw();
   
 });
 
 btnzoomneg.addEventListener("click", function() {         // Denna triggar file browser vid klick på knapp
   console.log("Klick zoom neg");
-  currScale = 0.5;
+  currScale = currScale - 0.5;
 
   ScaleAndRedraw();
 });
@@ -59,12 +105,11 @@ btnzoomneg.addEventListener("click", function() {         // Denna triggar file 
 function ScaleAndRedraw(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  console.log("X" + canvas.width + " Y" + canvas.height);
-  
-  ctx.drawImage(image, currImgX , currImgY, image.width * currScale ,image.height * currScale , currImgX , currImgY, image.width, image.height);
+  //ctx.drawImage(image, currImgX , currImgY, image.width * currScale ,image.height * currScale , currImgX , currImgY, image.width, image.height);
+  ctx.drawImage(image, 0 , 0, image.width ,image.height , currImgX , currImgY, image.width * currScale, image.height * currScale);
   //ctx.setTransform(1, 0, 0, 1, 0, 0);
 
-  currScale = 1;
+  //currScale = 1;
   
 }
 
@@ -94,11 +139,10 @@ function pegIt(){
   // Loggelilogg
   console.log(pixels);
 
-  var pixelation_level = canvas.width / pegs;
+  var pixelation_level = bas / stdPegs;
 
-  console.log(pixelation_level);
-  console.log(canvas.width);
-  console.log(pegs);
+  console.log("Pixelation level" + pixelation_level);
+  console.log("PegsW:" + pegsw + " PegsH:" + pegsh);
 
   for (let x = 0; x < canvas.width; x += pixelation_level) {
     for (let y = 0; y < canvas.height; y += pixelation_level) {
@@ -194,7 +238,10 @@ canvas.addEventListener('mousemove', e => {
       currImgX = currImgX - diffX;
       currImgY = currImgY - diffY;
 
-      ctx.drawImage(image, currImgX , currImgY , image.width, image.height);
+      //ctx.drawImage(image, currImgX , currImgY , image.width, image.height);
+      
+      ctx.drawImage(image, 0 , 0, image.width ,image.height , currImgX , currImgY, image.width * currScale, image.height * currScale);
+      
       mstartX = e.pageX;
       mstartY = e.pageY;
     }
@@ -225,11 +272,5 @@ image.addEventListener('load', function(){
 
   console.log("Offset -->" + image.width + " " + image.height);
 //-----------------------------------------------------------
-    
-
-    let particlesArray = [];
-    const numberOfParticles = 5000;
-    const detail = 1;
-
-    
+        
 });
